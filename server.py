46e9f3ae6f7eb48df5cbf608832f06df8eae2f98
Billy Andrews <scripture187@gmail.com>
2017-06-01 23:07:34 +0000

    initial commit

---
+++ server.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+
+@app.route('/')
+def mainIndex():
+	return render_template('index.html')
+
+# start the server
+if __name__ == '__main__':
+	app.run(host=os.getenv('IP', '0.0.0.0'), port =int(os.getenv('PORT', 8080)), debug=True)
